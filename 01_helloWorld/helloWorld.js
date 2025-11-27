0111bbf632630557cc56220f0a0f020c913a97c7
Tux-Garply <111070767+AIB1TAL0S@users.noreply.github.com>
2025-11-27 16:34:11 +0800

    changes

--- 01_helloWorld/helloWorld.js
+++ 01_helloWorld/helloWorld.js
@@ -1,9 +1,5 @@
 const helloWorld = function() {
-<<<<<<< Updated upstream
   return 'Hello World!';
-=======
-  return 'Hello, World!'
->>>>>>> Stashed changes
 };
 
 module.exports = helloWorld;
--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,19 +1,4 @@
-<<<<<<< Updated upstream
 const leapYears = function() {
-=======
-const leapYears = function(year) {
-/*
-FUNCTION IsLeapYear(year):
-   IF (year IS divisible by 4) AND (year IS NOT divisible by 100) THEN
-     RETURN TRUE
-   ELSE IF (year IS divisible by 400) THEN
-     RETURN TRUE
-   ELSE
-     RETURN FALSE
-   END IF
- END FUNCTION
-*/
->>>>>>> Stashed changes
     if((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0 )){
         return true;
     }else{
@@ -23,4 +8,3 @@ FUNCTION IsLeapYear(year):
 
 // Do not edit below this line
 module.exports = leapYears;
-//
