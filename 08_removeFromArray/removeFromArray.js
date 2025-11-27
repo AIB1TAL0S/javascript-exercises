7edb219d320ff3e26f311a39060be79448cf2366
Tux-Garply <111070767+AIB1TAL0S@users.noreply.github.com>
2025-11-27 16:32:53 +0800

    changes
    changes

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(...arraystorage) {
+    const removefromarray = arraystorage.pop();
+    const newarray = arraystorage.filter(item => item !== removefromarray);
+    return newarray;
 };
 
 // Do not edit below this line
